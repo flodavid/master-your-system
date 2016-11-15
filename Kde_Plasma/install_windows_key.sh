14f1503e9d653bdc438c4c81735be2213770c1f9
caminin <charlot.rodolphe@gmail.com>
2016-11-15 22:36:09 +0100

    add a script for plasma

---
+++ Kde_Plasma/install_windows_key.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+apt-get install git gcc make libx11-dev libxtst-dev pkg-config
+cd /usr/local/src
+git clone https://github.com/hanschen/ksuperkey.git
+cd ksuperkey
+make
+echo -e "#!/bin/bash \n /usr/local/src/ksuperkey" >> /etc/profile.d/ksuperkey.sh
+./ksuperkey
+
