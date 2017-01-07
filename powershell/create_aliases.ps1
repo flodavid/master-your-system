d143c569ef7483a5b3b8c85ee1e1e1d44488976b
Florian D <fl.david.53@gmail.com>
2017-01-07 17:07:58 +0100

    Powershell script to create aliases
    Three aliases are currently defined :
    + "gedit" for wordpad
    + "make" for mingw32-make (must be present in environment variables) 
    + "show" for "dir" or "ls" like command
    
    You can easily add more aliases, just add an item in the "aliases" array. The first parameter is the name of the alias, the second the command

---
+++ powershell/create_aliases.ps1
@@ -0,0 +1,36 @@
+﻿# Find the "My Documents" Folder
+$mydocuments = [Environment]::GetFolderPath("MyDocuments")
+$powershell_folder = $mydocuments + '\WindowsPowerShell'
+
+if (!(Test-Path $powershell_folder)) {
+    echo "Create folder : $powershell_folder"
+    New-Item $powershell_folder -type directory
+}
+
+$powershell_profile = $powershell_folder + '\Microsoft.PowerShell_profile.ps1'
+echo "Try to create file : $powershell_profile"
+
+
+if (Test-Path $powershell_profile) {
+    echo "Powershell profile file already exists. Aliases added nonetheless"
+}
+
+## Create aliases
+# Show command aliases
+$aliases = @(
+'New-Alias show Get-ChildItem'
+'New-Alias make mingw32-make'
+'New-Alias gedit write.exe')
+
+### Note : you can have a more gedit style  ###
+
+# Append to profile file:
+# Create the powershell profile file if it doesn't exists yet. Then, write into it to add aliases
+# This way, aliases are saved between Windows PowerShell sessions
+'' >> $powershell_profile
+for($i=0 ; $i -lt $aliases.Count ; $i++){
+    $aliases[$i] >> $powershell_profile
+}
+
+# Show all non-PowerShell aliases
+get-alias | where {$_.ReferencedCommand -match "exe"} | format-table Name,Definition -autosize
