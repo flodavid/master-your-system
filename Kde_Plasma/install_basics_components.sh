0462baa6fa894d4f06b453374167c6c829d80d52
caminin <charlot.rodolphe@gmail.com>
2017-12-12 15:31:11 +0100

    fix some things

--- Kde_Plasma/install_basics_components.sh
+++ Kde_Plasma/install_basics_components.sh
@@ -9,14 +9,15 @@ fi
 index=0
 to_install_rodolphe=('bumblebee' 'git' 'xinput' 'opera' 'nano' 'gparted');
 
-echo "To end this, just press Ctrl+C"
+echo -e "To end this, just tape quit\n"
 echo "You will be asked to install ${to_install_rodolphe[*]}"
 while true; do
-    read -p "Do you wish to install ${to_install_rodolphe[$index]}?" yn
-    case $yn in
-        [Yy]* ) zypper in $to_install_rodolphe[$index]; index=$index+1;;
+    read -p "Do you wish to install ${to_install_rodolphe[$index]}?" ynq
+    case $ynq in
+        [Yy]* ) zypper in ${to_install_rodolphe[$index]}; index=$index+1;;
         [Nn]* ) index=$index+1;;
-        * ) echo "Please answer yes or no.";;
+        [Qq]* ) break;;
+        * ) echo "Please answer yes or no or quit.";;
     esac
 done
 
